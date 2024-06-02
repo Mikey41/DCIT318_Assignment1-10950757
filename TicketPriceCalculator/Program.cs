bb61b98cf166f6b30818b2b4bbd6cd769c9c3d59
Michael-Angelo <mokonadu9021@gmail.com>
2024-06-02 08:28:49 +0000

    Created GradeCalculator, TicketPriceCalculator, and TriangleTypeIdentifier

--- TicketPriceCalculator/Program.cs
+++ TicketPriceCalculator/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 12 || age >= 65)
+                Console.WriteLine("Your ticket price is: GHC7");
+            else
+                Console.WriteLine("Your ticket price is: GHC10");
+        }
+    }
+}
--- TriangleTypeIdentifier/Program.cs
+++ TriangleTypeIdentifier/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side 1: ");
+            double side1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter side 2: ");
+            double side2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter side 3: ");
+            double side3 = double.Parse(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+                Console.WriteLine("The triangle is Equilateral");
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+                Console.WriteLine("The triangle is Isosceles");
+            else
+                Console.WriteLine("The triangle is Scalene");
+        }
+    }
+}
